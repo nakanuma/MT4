3743fcd7e5ade4383e091dba6fb025af1330e31b
nakanuma <k023g0121@g.neec.ac.jp>
2024-10-01 10:53:06 +0900

    ブランチ作成

--- main.cpp
+++ main.cpp
@@ -27,8 +27,6 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 	bool isFirstRightClick = true;
 	bool isFirstMiddleClick = true;
 
-
-
 	// ウィンドウの×ボタンが押されるまでループ
 	while (Novice::ProcessMessage() == 0) {
 		// フレームの開始
